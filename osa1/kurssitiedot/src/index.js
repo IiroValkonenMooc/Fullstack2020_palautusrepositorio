f80478666dc96af1095595c48cc987ddea3fd0e6
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-04-01 19:30:05 +0300

    tehtävä 1.4 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -10,6 +10,7 @@ const Header = (props) => {
 }
 
 const Part = (props) => {
+  console.log('osa tulostuu')
   console.log(props)
   return (
     <div>
@@ -19,45 +20,60 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
+  console.log('content tulostuu')
   console.log(props)
+  console.log(props.contentArray)
+  console.log(props.contentArray[0])
+  console.log(props.contentArray[0].name)
+
   return (
     <div>
-      <Part part={props.part1}/>
-      <Part part={props.part2}/>
-      <Part part={props.part3}/>
+      <Part part={props.contentArray[0]}/>
+      <Part part={props.contentArray[1]}/>
+      <Part part={props.contentArray[2]}/>
     </div>
   )
 }
 
 const Total = (props) => {
+  console.log(props)
+
+  let exerciseNumber = 0
+
+  props.contentArray.forEach(element => {
+    exerciseNumber += element.exercises
+  });
+
   return (
     <div>
-      <p>Number of exercises {props.part1.exercises + props.part2.exercises +props.part3.exercises }</p>
+      <p>Number of exercises {exerciseNumber }</p>
     </div>
   )
 }
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = {
-    name: 'Fundamentals of React',
-    exercises: 10
-  }
-  const part2 = {
-    name: 'Using props to pass data',
-    exercises: 7
-  }
-  const part3 = {
-    name: 'State of a component',
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
 
   return (
     <div>
       <Header course ={course} />
-      <Content part1={part1} part2={part2} part3={part3}  />
-      <Total part1={part1} part2={part2} part3={part3} />
+      <Content contentArray = {parts}  />
+      <Total contentArray = {parts} />
     </div>
   )
 }
