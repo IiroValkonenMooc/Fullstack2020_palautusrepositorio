dfe6b4b420ccda59cc07f09c508e6b648283a519
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-04-01 20:38:35 +0300

    tehtävä 1.5 tehty

--- osa1/kurssitiedot/src/index.js
+++ osa1/kurssitiedot/src/index.js
@@ -52,28 +52,30 @@ const Total = (props) => {
 }
 
 const App = () => {
-  const course = 'Half Stack application development'
-  const parts = [
-    {
-      name: 'Fundamentals of React',
-      exercises: 10
-    },
-    {
-      name: 'Using props to pass data',
-      exercises: 7
-    },
-    {
-      name: 'State of a component',
-      exercises: 14
-    }
-  ]
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
 
 
   return (
     <div>
-      <Header course ={course} />
-      <Content contentArray = {parts}  />
-      <Total contentArray = {parts} />
+      <Header course ={course.name} />
+      <Content contentArray = {course.parts}  />
+      <Total contentArray = {course.parts} />
     </div>
   )
 }
