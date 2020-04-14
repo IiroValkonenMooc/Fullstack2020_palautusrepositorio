e93c07a8ec8e1c6b61df4fae08d5c912da340adb
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-04-15 00:55:56 +0300

    tehtävät 1.8, 1.9 ja 1.10 tehty

--- osa1/unicafe/src/index.js
+++ osa1/unicafe/src/index.js
@@ -2,71 +2,85 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
+const Button = ({text, handleClick}) => {
+  //Miksi ei täällä?
+  // const addRating = () => {
+  //   console.log("add rating");
+  //   setter(rating + 1);
+  // }
+
+  return(
+    <button onClick={handleClick} >
+      {text}
+    </button>
+  )
+}
+
+const StatisticLine = ({text, value, optEndingChar}) => <p>{text} {value} {optEndingChar}</p>
+
+const Statistics = ({ good,  neutral, bad }) => {
+  let allRatings = good + neutral + bad;
+  let averageRating, positiveRatingPercentrage;
+  
+  if (good === 0 && bad === 0) {
+    console.log("can't calculate average");
+    averageRating = 0
+  } else {
+    console.log("calculating average");
+    averageRating = (good + (bad*(-1)))/allRatings;
+  }
+
+  if (good === 0) {
+    console.log("can't postive percentage");
+    positiveRatingPercentrage = 0;
+  } else {
+    console.log("calculating positive percentage");
+    positiveRatingPercentrage = (good / allRatings)*100 ;
+  }
+
+  if (allRatings > 0) {
+    return (
+      <div>
+        <h1>statistics</h1>
+
+        <p>good {good}</p>
+        <StatisticLine text="good" value={good} />
+        <p>neutral {neutral}</p>
+        <StatisticLine text="neutral" value={neutral} />
+        <p>bad {bad}</p>
+        <StatisticLine text="bad" value={bad} />
+        <p>all {allRatings}</p>
+        <StatisticLine text="all" value={allRatings} />
+        <p>average {averageRating} </p>
+        <StatisticLine text="average" value={averageRating} />
+        <p>positive {positiveRatingPercentrage} %</p>
+        <StatisticLine text="positive" value={positiveRatingPercentrage} optEndingChar="%" />
+      </div>
+    )
+  } else {
+    return (
+      <div>
+        <h1>statistics</h1>
+
+        <p>No feedback given</p>
+      </div>
+    )
+  }
+}
 
 const App = () => {
   const [ratingGood, setRatingGood] = useState(0)
   const [ratingNeutral, setRatingNeutral] = useState(0)
   const [ratingBad, setRatingBad] = useState(0)
-  const [allRatings, setRatingAllRatings] = useState(0)
-  const [averageRating, setAverageRating] = useState(0)
-  const [positiveRatingPercentrage, setPositiveRatingPercentrage] = useState(0)
-
-  let [tempGood, tempNeutral, tempBad] = [ratingGood, ratingNeutral, ratingBad]
-
-
-  //setAverageRating(((ratingGood)+(ratingNeutral*0.5))/allRatings )
-  const increaseRating = (ratin, setter) => {
-    console.log("add rating");
-    setter(ratin +1);
-    calculateStatistics()
-  }
-
-  const calculateStatistics = () => {
-    console.log("calc stats");
-    let tempAllRatings = tempGood + tempNeutral + tempBad
-    setRatingAllRatings( tempAllRatings);
-    
-    if (tempGood == 0 && tempBad == 0) {
-      console.log("can't calculate average");
-      setAverageRating(0)
-    } else {
-      console.log("calculating average");
-      setAverageRating((tempGood + (tempBad*(-1)))/tempAllRatings )
-    }
-
-    if (tempGood == 0) {
-      console.log("can't postive percentage");
-      setPositiveRatingPercentrage(0)
-    } else {
-      console.log("calculating positive percentage");
-      setPositiveRatingPercentrage(tempGood / tempAllRatings );
-    }
-  }
-  
-
 
   return (
     <div>
       <h1>give feedback</h1>
+      <Button text={"good"} handleClick={() => setRatingGood(ratingGood + 1) } />
+      <Button text={"neutral"} handleClick={() => setRatingNeutral(ratingNeutral + 1) } />
+      <Button text={"bad"} handleClick={() => setRatingBad(ratingBad + 1) } />
 
-      <button onClick={() => {tempGood++; increaseRating(ratingGood, setRatingGood);} }>
-        good
-      </button>
-      <button onClick={() => {tempNeutral++; increaseRating(ratingNeutral, setRatingNeutral); } } >
-        neutral
-      </button>
-      <button onClick={() => {tempBad++; increaseRating(ratingBad, setRatingBad)} } >
-        bad
-      </button>
-
-      <h1>statistics</h1>
-
-      <p>good {ratingGood}</p>
-      <p>neutral {ratingNeutral}</p>
-      <p>bad {ratingBad}</p>
-      <p>all {allRatings}</p>
-      <p>average {averageRating} </p>
-      <p>positive {positiveRatingPercentrage} %</p>
+      <Statistics good={ratingGood} neutral={ratingNeutral} bad={ratingBad} />
     </div>
   )
 }
