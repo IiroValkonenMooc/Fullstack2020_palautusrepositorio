69994dc87fe2ec49256b099a129a87735b3d4561
IiroValkonenMooc <62344585+IiroValkonenMooc@users.noreply.github.com>
2020-05-05 23:16:00 +0300

    tehtävä 2.13 tehty

--- osa2/maiden_tiedot/src/App.js
+++ osa2/maiden_tiedot/src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios'
+import CountryInfo from './components/CountryInfo'
 
 function App() {
+  const [searchFilter, setSearchFilter] = useState('')
+  const [countries, setCountries ] = useState([])
+
+  const hook = () => {
+    console.log('promise');
+    axios.get('https://restcountries.eu/rest/v2/all')
+      .then(response => {
+          console.log('response :>> ', response);
+          setCountries(response.data);
+        } 
+      )
+  }
+
+  useEffect(hook, [])
+
+  const handleChange = (event) => {
+      setSearchFilter(event.target.value)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      Find countries <input value={searchFilter} onChange = {handleChange} />
+      <CountryInfo countries = {countries} filter = {searchFilter} setFilter = {setSearchFilter} />
     </div>
   );
 }
---
+++ osa2/maiden_tiedot/src/components/CountryInfo.js
@@ -0,0 +1,65 @@
+import React from 'react'
+
+const CountryInfo = ({countries, filter, setFilter}) => {
+
+    let countriesFiltered = countries.slice().filter((country) => {
+        // console.log('country :>> ', country);
+        // console.log('country.name :>> ', country.name);
+
+        return country.name.toLowerCase().includes(filter.toLowerCase())
+    })
+
+    let removeSimilar = false;
+    let tempCountry = [];
+    countriesFiltered.forEach(country => {
+        if(country.name === filter ){
+            removeSimilar = true
+            tempCountry = country
+        }
+    });
+
+    if(removeSimilar){
+        countriesFiltered = [tempCountry]
+    }
+
+    console.log('countriesFiltered :>> ', countriesFiltered);
+
+    if (countriesFiltered.length === 1 ) {
+        console.log('Found only one!');
+
+        return(
+            <div>
+                <h2>{countriesFiltered[0].name}</h2>
+                <p>
+                    {countriesFiltered[0].capital} <br></br>
+                    population {countriesFiltered[0].population} 
+                </p>
+                <h3>Languages</h3>
+                <ul>
+                    {countriesFiltered[0].languages.map( (lang, index) => <li key= {index} > {lang.name} </li> ) }
+                </ul>
+                <br></br>
+                <img width = {300} src = {countriesFiltered[0].flag} alt = 'here should be a flag' ></img>
+            </div>
+        )
+    } else {
+        return (
+            <div>
+                {countriesFiltered.map( (country, index) => {
+                    return(
+                        <p key={index} >
+                            {country.name} 
+                            <button onClick = {() => setFilter(country.name) } >
+                                show
+                            </button>
+                            <br></br>
+                        </p>
+                    )
+                } ) }
+
+            </div>
+        )
+    }
+}
+
+export default CountryInfo
